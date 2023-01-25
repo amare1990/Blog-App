ba903822653083235cca775241aa6d5a3e0e3100
amare1990 <amaremek@gmail.com>
2023-01-26 00:34:58 +0300

    Implement update the post counter for a user method

--- app/models/post_record.rb
+++ app/models/post_record.rb
@@ -3,4 +3,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments, class_name: 'Comment', foreign_key: :post_id
   has_many :likes, class_name: 'Like', foreign_key: :post_id
+
+  after_save :update_posts_counter_for_user
+
+
+  def update_posts_counter_for_user
+    counter = Post.count('author_id')
+    author.update(counter)
+  end
 end
