cbd7f763ea1c59bfb90dc45059736027d73a99aa
amare1990 <amaremek@gmail.com>
2023-01-25 23:03:08 +0300

    create the Post model

---
+++ app/models/post_record.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_many :comments, class_name: 'Comment'
+  has_many :likes, class_name: 'Like'
+end
