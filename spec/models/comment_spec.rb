44993cb24a20cb92807c693f10487bd4195b81bd
amare1990 <amaremek@gmail.com>
2023-01-27 13:28:35 +0300

    Implement unit tests for the Comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  comment {
+    Comment.new(text: "Hi man", author_id: 1, post_id: 1)
+  }
+
+  before { comment.save }
+
+  describe "Comment model validation" do
+
+    t "Right now the comment model created should be valid" do
+      expect(comment).to be_valid
+    end
+
+    t "text value in the comment model should not be null" do
+      comment.text = nil
+      expect(comment).to_not be_valid
+    end
+
+    t "text value in the comment model should be equal to 'Hi man' " do
+      expect(comment.text).to eq('Hi man')
+    end
+  end # end of describe model
 end
