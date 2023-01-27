66b74faba137bdb5553ff3a67b3353dc9ef68412
amare1990 <amaremek@gmail.com>
2023-01-27 12:55:17 +0300

    Implement unit-test for user model class

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,35 +1,46 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  subject do
-    User.create(id: 1, name: 'Name', photo: 'Photo', bio: 'Bio')
-  end
+RSpec.describe User, type: model do
+  author { User.new(name: 'Amare', photo: 'photo.png', bio: 'Microverse student') }
 
-  before { subject.save }
+  before {author.save}
 
   describe 'Validations' do
-    it 'is not valid without a name' do
-      subject.name = nil
-      expect(subject).to_not be_valid
+
+    it 'Author now should be valid' do
+      expect(author).to be_valid
     end
 
-    it 'is valid with valid attributes' do
-      expect(subject).to be_valid
+    it 'name attribute value should be present' do
+      author.name = nil
+
+      expect(author).to_not be_valid
     end
 
-    it 'post_counter have to be bigger than 0' do
-      subject.posts_counter = -1
-      expect(subject).to_not be_valid
+    it 'photo attribute value should be present' do
+      author.photo = nil
+      expect(author).to_not be_valid
     end
 
-    it 'post_counter have to be greater or iqual to zero' do
-      subject.posts_counter = 0
-      expect(subject).to be_valid
+    it 'bio attribute value should be present' do
+      author.bio = nil
+      expect(author).to_not be_valid
     end
 
-    it 'latest_posts method should return the last 3 posts' do
-      4.times { Post.create(title: 'Title', text: 'Text', author_id: subject.id) }
-      expect(subject.latest_posts.count).to eq(3)
+    it 'post_counter befor the author posts should be zero' do
+      author.posts_counter = 0
+      expect(author).to be_valid
     end
-  end
+
+    it 'The `three_recent_posts` method should return the three latest post' do
+      Post.create(title: 'Job offer', text: 'Amare has got good job offer', author_id: author.id)
+      Post.create(title: 'Job offer2', text: 'Amare has got good job offer 2', author_id: author.id)
+      Post.create(title: 'Job offer 3', text: 'Amare has got good job offer 3', author_id: author.id)
+      Post.create(title: 'Job offer 4', text: 'Amare has got good job offer 4', author_id: author.id)
+      Post.create(title: 'Job offer 5', text: 'Amare has got good job offer 5', author_id: author.id)
+      expect(author.three_recent_posts.count).to eq(3)
+    end
+
+  end # end of descriobe block
+
 end
