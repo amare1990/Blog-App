65a99c6fa21efecc63b57b081f7b29715da500e0
amare1990 <amaremek@gmail.com>
2023-01-31 00:33:48 +0300

    Modify the routes.rb file to address for the PostsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  root 'users#index'
+
+  get 'users/user/:id', to: 'users#user'
+
+  resources :users do
+    resources :posts
+  end
 end
