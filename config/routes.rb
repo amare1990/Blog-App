d7e504dc36e5cb32a4dc393c6dee28a0b2b6b111
amare1990 <amaremek@gmail.com>
2023-01-31 12:20:36 +0300

    Modify the routes.rb file to stick to the project requirement

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
 
-
-  root 'users#index'
-
-  get 'users/user/:id', to: 'users#user'
-
-  resources :users do
-    resources :posts
+  resources :users, only: [:index, :show]do
+    resources :posts, only: [:index, :show]
   end
 end
