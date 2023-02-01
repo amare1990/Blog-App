97a7eeaf31d3f3d78270d86bd1e60d13c022f12b
amare1990 <amaremek@gmail.com>
2023-02-01 23:02:36 +0300

    Add @users and @user class instance variables in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
     render '/users/index'
   end
 
   def show
+    @user = User.find(params[:id])
     render 'users/show'
   end
 end
