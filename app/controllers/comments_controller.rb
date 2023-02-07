8ac9a2160c3756478fca08268c25dff49f86736b
amare1990 <amaremek@gmail.com>
2023-02-07 10:25:02 +0300

    Fix bugs on posts_counter, comments_counter and likes_counter fields

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,26 @@
 class CommentsController < ApplicationController
   def new
     @comment = Comment.new
-    @current_user = current_user
-    @comment.author = @current_user
-    @user = User.find(params[:user_id])
+    # @current_user = current_user
+    # @comment.author = @current_user
+    # @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
 
   def create
     @comment = Comment.new(comments_params)
     @current_user = current_user
-    @comment.author = current_user
+    post = Post.find(params[:id])
+    @comment.author = @current_user
+    @comment.post = post
+
+
 
     if @comment.save
-      redirect_to post_show_path(current_user)
+      flash.now[:notice] = "Commented successfully!"
+      redirect_to post_show_path(@current_user)
     else
+      flash.now[:error] = "Commenting failed!!"
       render 'new'
     end
   end
