7cdfcb6a5ccfd5ca9f7cb2947468628c48e731aa
amare1990 <amaremek@gmail.com>
2023-02-08 15:44:05 +0300

    Integ testing for the presence pf pagination when there are more postsmore than view

--- spec/features/posts_index_spec.rb
+++ spec/features/posts_index_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'post#index', type: :feature do
   describe 'post page' do
     before(:each) do
-      @user = User.create!(name: 'Amare23', photo: 'my-photo2', bio: 'Microverse student22')
+      @user = User.create!(name: 'Amare23', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Microverse student22')
       @post = Post.create!(title: 'Integ test22', text: 'differs much in using matchers...', author_id: @user.id)
       @comment = Comment.create!(text: 'I need help!', author: @user, post: @post)
       Like.create!(author_id: @user.id, post_id: @post.id)
@@ -13,6 +13,10 @@ RSpec.describe 'post#index', type: :feature do
 
     it " should display users' username" do
       expect(page).to have_content('Amare')
+    end
+
+    it "\n should display user's profile picture" do
+      expect(page).to have_css('img[src*="https://unsplash.com/photos/F_-0BxGuVvo"]')
     end
 
     it 'should display the number of posts for the user to be 1' do
@@ -47,9 +51,14 @@ RSpec.describe 'post#index', type: :feature do
       expect(page).to have_content('Likes: 1')
     end
 
+    it "\nI can see a section for pagination if there are more posts than fit on the view." do
+      expect(page).to have_content("Pagination")
+    end
+
     it "\nshould display post details when the user clicks title of the post link" do
       click_link 'Integ test22'
       expect(page).to have_current_path post_show_path(@post.author_id, @post)
     end
+
   end
 end
