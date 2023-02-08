8d4633e752aa793cb8fa4a8faf183c6caa81215a
gandradep <eventsonido@gmail.com>
2023-02-08 15:46:56 -0600

    Add controller filters and helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  protect_from_forgery prepend: true
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
 
-  # def current_user
-  #   @current_user = User.first
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password password_confirmation])
+  end
 end
