f2c0e74e7a4f2b7877015ca251c0f633c22db83a
amare1990 <amaremek@gmail.com>
2023-02-08 23:32:39 +0300

    Run all migration files as new

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   validates :photo, presence: true
   validates :bio, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :email, uniqueness: { case_sensitive: false }
 
   def three_recent_posts
     posts.order(created_at: :desc).limit(3)
