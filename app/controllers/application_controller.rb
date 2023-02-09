3fd7f7f1211eb3f3271ea2e52ece7e4c196b87b6
amare1990 <amaremek@gmail.com>
2023-02-09 19:37:06 +0300

    Add additional bio and photo registration fileds in the ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio email password password_confirmation])
     devise_parameter_sanitizer.permit(:sign_in, keys: %i[name password])
     # params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
