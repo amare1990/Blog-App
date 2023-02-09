a63f0732da24247c394c9fa6f3f6b01fb9713e3b
amare1990 <amaremek@gmail.com>
2023-02-09 15:28:20 +0300

    Edit the keys in the configured permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,25 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  protect_from_forgery prepend: true
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
+    # params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
+
+  # def devise_parameter_sanitizer
+  #   if resource_class == User
+  #     User::ParameterSanitizer.new(User, :user, params)
+  #   else
+  #     super # Use the default one
+  #   end
+  # end
+
 end
+
+
+
